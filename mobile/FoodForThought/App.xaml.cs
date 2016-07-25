be4d7ae1fb5c59ac07083b3398519c6dedf4c2a4
Chris Risner <chris@chrisrisner.com>
2016-07-25 15:19:23 -0700

    Initial commit

---
+++ mobile/FoodForThought/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FoodForThought.Abstractions;
+using FoodForThought.Services;
+using Xamarin.Forms;
+
+
+namespace FoodForThought
+{
+	public partial class App : Application
+	{
+		public static ICloudService CloudService { get; set; }
+		public static Guid DeviceId { get; private set; }
+
+		public App()
+		{
+			InitializeComponent();
+
+			LoadDeviceId();
+
+			// Initialize the Cloud Service
+			CloudService = new AzureCloudService();
+
+			//MainPage = new FoodForThoughtPage();
+			MainPage = new NavigationPage(new Pages.EntryPage());
+		}
+
+		private void LoadDeviceId()
+		{
+			if (Application.Current.Properties.ContainsKey("DeviceId"))
+			{
+				DeviceId = Guid.Parse(Application.Current.Properties["DeviceId"].ToString());
+			}
+			else {
+				DeviceId = Guid.NewGuid();
+				Application.Current.Properties["DeviceId"] = DeviceId.ToString();
+				Application.Current.SavePropertiesAsync();
+			}
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ mobile/FoodForThought/ViewModels/EntryPageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using FoodForThought.Abstractions;
+using Xamarin.Forms;
+
+
+namespace FoodForThought.ViewModels
+{
+	public class EntryPageViewModel : BaseViewModel
+	{
+		public string ZipCode { get; set; }
+
+		public EntryPageViewModel()
+		{
+			 Title = "Food for Thought";
+			if (Application.Current.Properties.ContainsKey("ZipCode"))
+			{
+				ZipCode = Application.Current.Properties["ZipCode"].ToString();
+			}
+			else {
+				ZipCode = "98033";
+			}
+		}
+
+		Command loginCmd;
+		public Command LoginCommand => loginCmd ?? (loginCmd = new Command(async () => await ExecuteLoginCommand()));
+
+		async Task ExecuteLoginCommand()
+		{
+			if (IsBusy)
+				return;
+			IsBusy = true;
+
+			try
+			{				
+				Application.Current.Properties["ZipCode"] = ZipCode;
+				Application.Current.SavePropertiesAsync();
+
+
+				//Application.Current.MainPage = new NavigationPage(new Pages.MainPageCS());
+				Application.Current.MainPage = new Pages.MasterPage();
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine($"[ExecuteLoginCommand] Error = {ex.Message}");
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
+	}
+}
+
